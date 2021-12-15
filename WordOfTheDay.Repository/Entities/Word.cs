f529c099d6a79f405a5f0482273e9ff04a4af3d0
Ilya Chudinovskikh <87187545+Ilya-Chudinovskikh@users.noreply.github.com>
2021-12-15 11:04:14 +0300

    3-layers architecture was added

---
+++ WordOfTheDay.Repository/Entities/Word.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WordOfTheDay.Entities
+{
+    public class Word
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Enter your email!")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Enter your word!")]
+        [StringLength(50, ErrorMessage = "Your word must be no more than 50 characters long!")]
+        public string Text { get; set; }
+        [Required]
+        public DateTime AddTime { get; set; }
+    }
+}
---
+++ WordOfTheDay.Repository/Entities/WordContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WordOfTheDay.Entities
+{
+    public class WordContext : DbContext
+    {
+        public WordContext (DbContextOptions<WordContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Word> Words { get; set; }
+    }
+}
