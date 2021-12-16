c570c29865d4c9a80ed45cb34af788926c7d122b
Ilya Chudinovskikh <87187545+Ilya-Chudinovskikh@users.noreply.github.com>
2021-12-16 16:09:27 +0300

    All requsted functions were implemented.

--- WordOfTheDay.PerformanceCheck/WordOfTheDay.PerformanceCheck/PerformanceCheck.cs
+++ WordOfTheDay.PerformanceCheck/WordOfTheDay.PerformanceCheck/PerformanceCheck.cs
@@ -2,10 +2,13 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Diagnostics;
 using WordOfTheDay.Entities;
 
 namespace PerformanceCheck
@@ -14,63 +17,82 @@ namespace PerformanceCheck
     {
         static async Task Main()
         {
-            Request request = new(1, @"C:\Users\Илья\OneDrive\Документы\check.txt");
-            await Request.MakeRequest(Request.GetRandomEmail(), Request.GetRandomWord(Request.GetWords(request)));
+            Request request = new(20, @"C:\Users\Илья\OneDrive\Документы\check.txt");
+            await request.MinMaxAvgTime();
         }
-        public class Request
+    }
+    public class Request
+    {
+        int AmountOfQueries { get; set; }
+        string WordsFile { get; set; }
+        public Request(int amountOfQueries, string wordsFile)
         {
-            int amountOfQueries { get; set; }
-            string wordsFile { get; set; }
-            public Request(int _amountOfQueries, string _wordsFile)
+            AmountOfQueries = amountOfQueries;
+            WordsFile = wordsFile;
+        }
+        public List<string> GetWords()
+        {
+            var words = new List<string>();
+
+            using (StreamReader sr = new(WordsFile))
             {
-                amountOfQueries = _amountOfQueries;
-                wordsFile = _wordsFile;
+                words = sr.ReadToEnd().Split().ToList();
             }
-            public static List<string> GetWords(Request request)
+
+            return words;
+        }
+        public static string GetRandomWord(List<string> words)
+        {
+            Random random = new();
+
+            string randomString = words[random.Next(0, words.Count)];
+
+            return randomString;
+        }
+        public static string GetRandomEmail()
+        {
+            Random random = new();
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            string[] emails = { "@email.com", "@itransition.com", "@yandex.ru" };
+            string email = null;
+
+            for (int i = 0; i < random.Next(6, 13); i++)
+                email += chars[random.Next(chars.Length)].ToString().ToLower();
+
+            return email += emails[random.Next(emails.Length)];
+        }
+        public static async Task MakeRequest(string email, string text)
+        {
+            var url = "https://localhost:5001/api/words";
+            using var httpClient = new HttpClient();
+            var newWord = new Word() { Email = email, Text = text };
+            var response = await httpClient.PostAsJsonAsync(url, newWord);
+            if (!response.IsSuccessStatusCode)
             {
-                var words = new List<string>();
-
-                using (StreamReader sr = new(request.wordsFile))
-                {
-                    words = sr.ReadToEnd().Split().ToList();
-                }
-
-                return words;
+                Console.WriteLine("Request failed.");
             }
-            public static string GetRandomWord(List<string> words)
+        }
+        public async Task<List<float>> MeasureRequestTime()
+        {
+            var measurements = new List<float>();
+            for (int i = 0; i < AmountOfQueries; i++)
             {
-                Random random = new();
-
-                string randomString = words[random.Next(0, words.Count)];
-
-                return randomString;
+                var watch = new Stopwatch();
+                string email = GetRandomEmail();
+                string text = GetRandomWord(GetWords());
+                watch.Start();
+                await MakeRequest(email, text);
+                watch.Stop();
+                measurements.Add(watch.ElapsedMilliseconds);
             }
-            public static string GetRandomEmail()
-            {
-                Random random = new();
-                string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-                string[] emails = { "@email.com", "@itransition.com", "@yandex.ru" };
-                string email = null;
-
-                for (int i = 0; i < random.Next(6, 13); i++)
-                    email += chars[random.Next(chars.Length)].ToString().ToLower();
-
-                return email += emails[random.Next(emails.Length)];
-            }
-            public static async Task MakeRequest(string email, string text)
-            {
-                var url = "https://localhost:5001/api/words";
-                var jsonSerializerOptions = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
-                using (var httpClient = new HttpClient())
-                {
-                    var newWord = new Word() { Email = email, Text = text };
-                    var response = await httpClient.PostAsJsonAsync(url, newWord);
-                    if (!response.IsSuccessStatusCode)
-                    {
-                        Console.WriteLine("Request failed.");
-                    }
-                }
-            }
+            return measurements;
+        }
+        public async Task MinMaxAvgTime()
+        {
+            var measurements = await MeasureRequestTime();
+            Console.WriteLine("Max time for request: " + measurements.Max() + "\n" +
+                "Min time for request: " + measurements.Min() + "\n" +
+                "Average time for request: " + measurements.Average() + "\n");
         }
     }
 }
