0cc8e9c3c1cce43a1dcb7e329c9f9ed578f71f70
Ilya Chudinovskikh <87187545+Ilya-Chudinovskikh@users.noreply.github.com>
2022-01-19 21:46:44 +0300

    Docker-compose file was created.

--- WordOfTheDay.Api/Program.cs
+++ WordOfTheDay.Api/Program.cs
@@ -14,6 +14,7 @@ namespace WordOfTheDay
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*80");
                     webBuilder.UseStartup<Startup>();
                 });
     }
